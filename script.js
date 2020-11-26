6341082647a1b2e9fe0ee5e6b856295bae41a9b7
kula <kula124@gmail.com>
2020-11-26 22:29:56 +0100

    JavaScript za tww

---
+++ script.js
@@ -0,0 +1,11 @@
+function idiNaAbout() {
+  document.getElementById("about").scrollIntoView();
+}
+
+function idiNaNesto() {
+  document.getElementById("nesto").scrollIntoView();
+}
+
+function idiNaVrh() {
+  window.scrollTo(0, 0);
+}
